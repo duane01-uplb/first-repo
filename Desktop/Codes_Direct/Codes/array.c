7efafd32b861cb6ff517815ae4930b84c6bbd085
duane01-uplb <dsmendoza5@up.edu.ph>
2025-03-13 09:23:58 +0800

    Added array.c

---
+++ Desktop/Codes_Direct/Codes/array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+    float *grades;
+    int i, size = 3, choice = 0;
+
+    grades = (float *) malloc(sizeof(float) * size);
+
+    while (choice != 1){
+    for(i=0; i<size; i++) {
+        printf("Enter grade %d: ", i+1);
+        scanf("%f", &grades[i]);
+    }
+    for(i=0; i<size; i++) {
+        printf("Grade %d: %.2f\n", i+1, grades[i]);
+    }
+    printf("Do you want to continue? (0/1): ");
+    scanf("%d", &choice);
+    }
+
+    free(grades);
+    return 0;
+}
